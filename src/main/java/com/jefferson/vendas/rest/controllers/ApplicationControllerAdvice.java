756f65c323463fcfc3211d87cbfd90431e82cabf
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-10-04 21:09:26 -0300

    controller advice e exception handler

---
+++ src/main/java/com/jefferson/vendas/rest/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.jefferson.vendas.rest.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.jefferson.vendas.exceptions.RegraNegocioException;
+import com.jefferson.vendas.rest.ApiErrors;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+	@ExceptionHandler(RegraNegocioException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+		String errorMessage = ex.getMessage();
+		
+		return new ApiErrors(errorMessage);
+	}
+	
+}
