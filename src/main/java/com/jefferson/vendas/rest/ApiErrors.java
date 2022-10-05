559144403de12203690a06d1b7b59a1dc194c637
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-10-05 20:20:41 -0300

    construtor com lista de errors (string)

--- src/main/java/com/jefferson/vendas/rest/ApiErrors.java
+++ src/main/java/com/jefferson/vendas/rest/ApiErrors.java
@@ -6,6 +6,10 @@ import java.util.List;
 public class ApiErrors {
 
 	private List<String> errors;
+
+	public ApiErrors(List<String> errorMessage) {
+		this.errors = errorMessage;
+	}
 	
 	public ApiErrors(String errorMessage) {
 		this.errors = Arrays.asList(errorMessage);
