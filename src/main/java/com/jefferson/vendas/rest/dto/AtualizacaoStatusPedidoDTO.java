fbf563989c908eaf58ba49e6635ef65e29da3933
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-10-05 19:53:05 -0300

    DTO para atualizar status do pedido

---
+++ src/main/java/com/jefferson/vendas/rest/dto/AtualizacaoStatusPedidoDTO.java
@@ -0,0 +1,15 @@
+package com.jefferson.vendas.rest.dto;
+
+public class AtualizacaoStatusPedidoDTO {
+	private String novoStatus;
+
+	public String getNovoStatus() {
+		return this.novoStatus;
+	}
+
+	public void setNovoStatus(String novoStatus) {
+		this.novoStatus = novoStatus;
+	}
+	
+	
+}
