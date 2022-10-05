cdcd4e7a6c1b1246a21acd46aa1ddd42da17910c
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-10-05 19:49:58 -0300

    adicionando atributo de status

--- src/main/java/com/jefferson/vendas/domain/entities/Pedido.java
+++ src/main/java/com/jefferson/vendas/domain/entities/Pedido.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -13,6 +15,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.jefferson.vendas.domain.enums.StatusPedido;
 
 @Entity
 @Table(name = "pedido")
@@ -32,6 +36,10 @@ public class Pedido {
 	
 	@Column(name = "total", precision = 20, scale = 2)
 	private BigDecimal total;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status")
+	private StatusPedido status;
 	
 	@OneToMany(mappedBy = "pedido")
 	private List<ItemPedido> itens;
@@ -64,6 +72,14 @@ public class Pedido {
 	
 	public BigDecimal getTotal() {
 		return this.total;
+	}
+	
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
+	
+	public StatusPedido getStatus() {
+		return this.status;
 	}
 	
 	public void setTotal(BigDecimal total) {
