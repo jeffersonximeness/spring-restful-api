1052bc138deec7efbba45bc05acd6c3d4f57869f
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-10-05 20:20:59 -0300

    exception handler para argumento da requisicao nao valido

--- src/main/java/com/jefferson/vendas/rest/controllers/ApplicationControllerAdvice.java
+++ src/main/java/com/jefferson/vendas/rest/controllers/ApplicationControllerAdvice.java
@@ -1,6 +1,10 @@
 package com.jefferson.vendas.rest.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,5 +30,16 @@ public class ApplicationControllerAdvice {
 		String errorMessage = ex.getMessage();
 		
 		return new ApiErrors(errorMessage);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex) {
+		List<String> errors = ex.getBindingResult().getAllErrors()
+				.stream()
+				.map(error -> error.getDefaultMessage())
+				.collect(Collectors.toList());
+		
+		return new ApiErrors(errors);
 	}
 }
