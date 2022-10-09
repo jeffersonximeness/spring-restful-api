a08bfc0e4ad8c0e4ddf0885cde509baebe278bea
Jefferson Ximenes <jeffersonximenes1@gmail.com>
2022-10-09 18:40:48 -0300

    configurando swagger

---
+++ src/main/java/com/jefferson/vendas/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.jefferson.vendas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.jefferson.vendas.rest.controllers"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(this.apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Vendas API")
+				.description("Api do projeto de Vendas")
+				.version("1.0")
+				.contact(this.contact())
+				.build();
+	}
+	
+	private Contact contact() {
+		return new Contact("Jefferson Ximenes", "http://github.com/jeffersonximeness", "jeffersonximenes1@gmail.com");
+	}
+}
